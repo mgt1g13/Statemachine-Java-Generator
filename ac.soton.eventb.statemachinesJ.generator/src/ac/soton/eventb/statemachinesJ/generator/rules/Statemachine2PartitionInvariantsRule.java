a9e447cecc281f98111d6d50c8f886136c19d460
mgt1g13 <mgt1g13@soton.ac.uk>
2014-06-19 15:48:11 +0100

    Bug Fix: Was not considering pre-conditions stated in the QVTo transform

--- ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/Statemachine2PartitionInvariantsRule.java
+++ ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/Statemachine2PartitionInvariantsRule.java
@@ -39,10 +39,21 @@ public class Statemachine2PartitionInvariantsRule extends AbstractRule  implemen
 		EventBNamedCommentedComponentElement container = (EventBNamedCommentedComponentElement)EcoreUtil.getRootContainer(sourceElement);
 		Statemachine sourceSm = (Statemachine) sourceElement;
 		
-		if(Utils.isRootStatemachine(sourceSm))
-			ret.add(Make.descriptor(container, invariants, partitionInv4RootSM(sourceSm), 5));
-		else
-			ret.add(Make.descriptor(container, invariants, partitionInv4NestedSM(sourceSm), 5));
+		int numberOfStates = 0;
+		
+		for(AbstractNode abs : sourceSm.getNodes()){
+			if(abs instanceof State)
+				numberOfStates++;
+		}
+		
+		if(Utils.isRootStatemachine(sourceSm)){
+			if(sourceSm.getRefines() == null && numberOfStates > 1)
+				ret.add(Make.descriptor(container, invariants, partitionInv4RootSM(sourceSm), 5));
+		}
+		else{
+			if(sourceSm.getRefines() == null && numberOfStates > 1)
+				ret.add(Make.descriptor(container, invariants, partitionInv4NestedSM(sourceSm), 5));
+		}
 		return ret;
 		
 	}
