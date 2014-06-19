7c943ea3d9052d576f9bc3d27d890dd887f08088
mgt1g13 <mgt1g13@soton.ac.uk>
2014-06-19 16:36:51 +0100

    Some priorities fixed

--- ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/State2SubstateInvariantRule.java
+++ ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/State2SubstateInvariantRule.java
@@ -41,7 +41,7 @@ public class State2SubstateInvariantRule extends AbstractRule  implements IRule 
 			State parentState = (State) sourceState.eContainer().eContainer();
 			Invariant newInvariant = Make.invariant(sourceState.getName() + Strings._SUBSTATEOF_ + parentState.getName(),
 									Utils.parenthesize(sourceState.getName() + Strings.B_EQ + Strings.B_TRUE) + Strings.B_IMPL + Utils.parenthesize(parentState.getName() + Strings.B_EQ + Strings.B_TRUE), "");
-			ret.add(Make.descriptor(container, invariants, newInvariant, 8));
+			ret.add(Make.descriptor(container, invariants, newInvariant, 10));
 		}
 		
 
