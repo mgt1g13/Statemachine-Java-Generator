1a7ec8b5254eb06dacffe6e47830cb7ab0a170cc
mgt1g13 <mgt1g13@soton.ac.uk>
2014-06-20 17:37:55 +0100

    Changed Substate Invariant rule to make use of the enabled method

--- ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/State2SubstateInvariantRule.java
+++ ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/State2SubstateInvariantRule.java
@@ -19,13 +19,12 @@ import ac.soton.eventb.statemachinesJ.generator.utils.Utils;
 
 public class State2SubstateInvariantRule extends AbstractRule  implements IRule {
 
-	
-	@Override
-	public boolean dependenciesOK(EventBElement sourceElement, final List<GenerationDescriptor> generatedElements) throws Exception  {
-		return true;
-	
+		
+	public boolean enabled(EventBElement sourceElement) throws Exception{
+		State sourceState = (State) sourceElement;
+		return (!(sourceState.eContainer().eContainer() instanceof MachineImpl) && sourceState.getRefines() == null); //If it is not a state from the root statemachine
 	}
-
+	
 	
 	/**
 	 * States2SubstateInvariants
@@ -37,15 +36,11 @@ public class State2SubstateInvariantRule extends AbstractRule  implements IRule 
 		List<GenerationDescriptor> ret = new ArrayList<GenerationDescriptor>();
 		EventBNamedCommentedComponentElement container = (EventBNamedCommentedComponentElement)EcoreUtil.getRootContainer(sourceElement);
 		State sourceState = (State)sourceElement;
-		if(!(sourceState.eContainer().eContainer() instanceof MachineImpl) && sourceState.getRefines() == null){//If it is not a state from the root statemachine
-			State parentState = (State) sourceState.eContainer().eContainer();
-			Invariant newInvariant = Make.invariant(sourceState.getName() + Strings._SUBSTATEOF_ + parentState.getName(),
-									Utils.parenthesize(sourceState.getName() + Strings.B_EQ + Strings.B_TRUE) + Strings.B_IMPL + Utils.parenthesize(parentState.getName() + Strings.B_EQ + Strings.B_TRUE), "");
-			ret.add(Make.descriptor(container, invariants, newInvariant, 10));
-		}
-		
+		State parentState = (State) sourceState.eContainer().eContainer();
+		Invariant newInvariant = Make.invariant(sourceState.getName() + Strings._SUBSTATEOF_ + parentState.getName(),
+				Utils.parenthesize(sourceState.getName() + Strings.B_EQ + Strings.B_TRUE) + Strings.B_IMPL + Utils.parenthesize(parentState.getName() + Strings.B_EQ + Strings.B_TRUE), "");
+		ret.add(Make.descriptor(container, invariants, newInvariant, 10));
 
-		
 		return ret;
 		
 	}
