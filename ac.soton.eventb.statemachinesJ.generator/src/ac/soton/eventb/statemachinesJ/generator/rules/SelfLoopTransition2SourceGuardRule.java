034a6658c3f5692e8255759bd7be73fa25201847
mgt1g13 <mgt1g13@soton.ac.uk>
2014-06-24 10:32:23 +0100

    Added rule for generation fo source guards on SelfLooping events. Unused depencies removed

---
+++ ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/rules/SelfLoopTransition2SourceGuardRule.java
@@ -0,0 +1,60 @@
+package ac.soton.eventb.statemachinesJ.generator.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eventb.emf.core.EventBElement;
+import org.eventb.emf.core.machine.Event;
+import org.eventb.emf.core.machine.Guard;
+
+import ac.soton.eventb.emf.diagrams.generator.AbstractRule;
+import ac.soton.eventb.emf.diagrams.generator.GenerationDescriptor;
+import ac.soton.eventb.emf.diagrams.generator.IRule;
+import ac.soton.eventb.emf.diagrams.generator.utils.Make;
+import ac.soton.eventb.statemachines.State;
+import ac.soton.eventb.statemachines.Transition;
+import ac.soton.eventb.statemachinesJ.generator.strings.Strings;
+import ac.soton.eventb.statemachinesJ.generator.utils.Utils;
+
+public class SelfLoopTransition2SourceGuardRule extends AbstractRule  implements IRule {
+
+	@Override
+	public boolean enabled(EventBElement sourceElement) throws Exception{
+		return Utils.isSelfLoop((Transition) sourceElement) || ((Transition) sourceElement).getSource() instanceof State; 
+	}
+	
+
+	@Override
+	public boolean dependenciesOK(EventBElement sourceElement, final List<GenerationDescriptor> generatedElements) throws Exception  {
+		return true;
+	
+	}
+
+	
+	/**
+	 * SelfLoopTransition2Guard
+	 * 
+	 * Generates guard for self looping events
+	 */
+	@Override
+	public List<GenerationDescriptor> fire(EventBElement sourceElement, List<GenerationDescriptor> generatedElements) throws Exception {
+		List<GenerationDescriptor> ret = new ArrayList<GenerationDescriptor>();
+		Transition sourceTransition = (Transition) sourceElement;
+		State sourceState = (State) sourceTransition.getSource();
+		
+		String name = Strings.ISIN_ + sourceState.getName();
+		String predicate = sourceState.getName() + Strings.B_EQ + Strings.B_TRUE;
+		
+		Guard grd = (Guard) Make.guard(name, predicate);
+		
+		for(Event e : sourceTransition.getElaborates()){
+			ret.add(Make.descriptor(e, guards, grd, 10));
+			
+		}
+		
+		return ret;
+	
+		
+	}
+
+}
