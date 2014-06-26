659dcb637177f6e287c04a40f45605dd4d635ae0
mgt1g13 <mgt1g13@soton.ac.uk>
2014-06-26 18:06:43 +0100

    Added generation of typing axioms for the generated Context

---
+++ ac.soton.eventb.statemachinesJ.generator/src/ac/soton/eventb/statemachinesJ/generator/enumRules/State2TypingAxiomRule.java
@@ -0,0 +1,60 @@
+package ac.soton.eventb.statemachinesJ.generator.enumRules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eventb.emf.core.EventBElement;
+import org.eventb.emf.core.EventBNamedCommentedComponentElement;
+import org.eventb.emf.core.context.Axiom;
+import org.eventb.emf.core.context.Context;
+
+import ac.soton.eventb.emf.diagrams.generator.AbstractRule;
+import ac.soton.eventb.emf.diagrams.generator.GenerationDescriptor;
+import ac.soton.eventb.emf.diagrams.generator.IRule;
+import ac.soton.eventb.emf.diagrams.generator.utils.Find;
+import ac.soton.eventb.emf.diagrams.generator.utils.Make;
+import ac.soton.eventb.statemachines.State;
+import ac.soton.eventb.statemachines.TranslationKind;
+import ac.soton.eventb.statemachinesJ.generator.strings.Strings;
+import ac.soton.eventb.statemachinesJ.generator.utils.Utils;
+
+public class State2TypingAxiomRule extends AbstractRule implements IRule{
+
+	/**
+	 * Only enabled for enumeration translation
+	 */
+	@Override
+	public boolean enabled(EventBElement sourceElement) throws Exception  {
+		return Utils.getRootStatemachine((State) sourceElement).getTranslation().equals(TranslationKind.SINGLEVAR);
+	}
+
+	/**
+	 * Waits until context has not being generated
+	 */
+	@Override
+	public boolean dependenciesOK(EventBElement sourceElement, final List<GenerationDescriptor> generatedElements) throws Exception  {
+		EventBNamedCommentedComponentElement container = (EventBNamedCommentedComponentElement)EcoreUtil.getRootContainer(sourceElement);
+		return Find.generatedElement(generatedElements, Find.project(container), components, Strings.CTX_NAME(container)) != null;
+	}
+	
+	/**
+	 * Generates typing axioms from a State
+	 */
+	@Override
+	public List<GenerationDescriptor> fire(EventBElement sourceElement, List<GenerationDescriptor> generatedElements) throws Exception {
+		List<GenerationDescriptor> ret = new ArrayList<GenerationDescriptor>();
+		EventBNamedCommentedComponentElement container = (EventBNamedCommentedComponentElement)EcoreUtil.getRootContainer(sourceElement);
+		
+		State sourceState = (State) sourceElement;
+		Context ctx = (Context)Find.generatedElement(generatedElements, Find.project(container), components, Strings.CTX_NAME(container));
+		
+		Axiom newSet = (Axiom) Make.axiom(Strings.TYPEOF_ + sourceState.getName(),
+				sourceState.getName() + Strings.B_IN + Utils.getStatemachine(sourceState).getName() + Strings._STATES,
+				"");
+		
+		ret.add(Make.descriptor(ctx, axioms, newSet, 1));
+		return ret;
+	}
+	
+}
